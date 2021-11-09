35147ca4a3c867e5ef32e73746906fecd4418821
jessicacorreaes <jessicacorreaes@gmail.com>
2021-11-09 19:19:35 -0300

    Classe Conta e CriarConta

---
+++ src/main/java/br/com/panacademy/bluebank/CriarConta.java
@@ -0,0 +1,35 @@
+package br.com.panacademy.bluebank;
+
+package br.com.panacademy.bluebank;
+
+public class CriarConta {
+
+    public static void main(String[] args) {
+
+        Conta primeiraConta = new Conta(01, 2020,200.00);
+        primeiraConta.deposita(200);
+        System.out.println(primeiraConta.getSaldo());
+
+        Conta segundaConta = new Conta(02, 1010,200.00);
+        segundaConta.deposita(200);
+        System.out.println(segundaConta.getSaldo());
+
+//        //transfere
+//        primeiraConta.transfere(100, segundaConta);
+
+        boolean sucessoTransferencia = primeiraConta.transfere(100, segundaConta);
+
+        if (sucessoTransferencia) {
+            System.out.println("transferencia com sucesso");
+
+        } else {
+            System.out.println("faltou dinheiro");
+
+        }
+
+        System.out.println("primeira conta tem " + primeiraConta.getSaldo());
+        System.out.println("segunda conta tem " + segundaConta.getSaldo());
+
+    }
+
+}
