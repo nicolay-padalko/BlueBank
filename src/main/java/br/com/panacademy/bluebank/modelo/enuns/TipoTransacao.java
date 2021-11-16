dbfa172571c32ccd572f4407976af14ec3380422
Joanantha Matheus <jmvg.comp@gmail.com>
2021-11-16 01:21:57 -0300

    Pacote de enum movido para o pacote de modelo

---
+++ src/main/java/br/com/panacademy/bluebank/modelo/enuns/TipoTransacao.java
@@ -0,0 +1,14 @@
+package br.com.panacademy.bluebank.modelo.enuns;
+
+public enum TipoTransacao {
+
+    DEPOSITO("Deposito"),
+    SAQUE("Saque"),
+    TRANSFERENCIA("Transferencia");
+
+
+    TipoTransacao(String descricao) {
+    }
+
+}
+
