2eae2d34eca86d2b271005805d6111e78cbefd61
jessicacorreaes <jessicacorreaes@gmail.com>
2021-11-17 21:07:26 -0300

    teste2

--- src/main/java/br/com/panacademy/bluebank/controle/ClienteControle.java
+++ src/main/java/br/com/panacademy/bluebank/controle/ClienteControle.java
@@ -1,12 +1,12 @@
 package br.com.panacademy.bluebank.controle;
 
 import br.com.panacademy.bluebank.dto.ClienteDTO;
-import br.com.panacademy.bluebank.repositorio.ClienteRepositorio;
 import br.com.panacademy.bluebank.servico.ClienteServico;
 import org.apache.coyote.Response;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
