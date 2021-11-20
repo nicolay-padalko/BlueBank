23eeede6666eea1c6ff58766a40867c58034d885
Joanantha Matheus <jmvg.comp@gmail.com>
2021-11-20 17:49:31 -0300

    Criação do método salvar de cliente

--- src/main/java/br/com/panacademy/bluebank/controle/ClienteControle.java
+++ src/main/java/br/com/panacademy/bluebank/controle/ClienteControle.java
@@ -3,9 +3,13 @@ package br.com.panacademy.bluebank.controle;
 import br.com.panacademy.bluebank.dto.ClienteDTO;
 import br.com.panacademy.bluebank.modelo.Cliente;
 import br.com.panacademy.bluebank.servico.ClienteServico;
+import ch.qos.logback.core.net.server.Client;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,9 +29,11 @@ public class ClienteControle {
     }
 
     @PostMapping
-    public ResponseEntity<Cliente> salvarCliente(Cliente cliente){
-        Cliente clienteSalvo = clienteServico.salvarCliente(cliente);
-        return ResponseEntity.ok(clienteSalvo);
+    public ResponseEntity<ClienteDTO> salvarCliente(@RequestBody Cliente cliente){
+        ClienteDTO clienteDTO = clienteServico.salvarCliente(cliente);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(clienteDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(clienteDTO);
     }
 
     @DeleteMapping(value = "/{id}") //mapear a url
@@ -36,7 +42,7 @@ public class ClienteControle {
         return ResponseEntity.noContent().build();
     }
 
-    @PutMapping("{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<ClienteDTO> update(@PathVariable Long id, @RequestBody ClienteDTO dto) {
         dto = clienteServico.editar(id, dto);
         return ResponseEntity.ok().body(dto);
