af2ac0e1b4ac1f51344a66c4fb7b273dcb1309cd
rsbastos <rene.s.bastos@gmail.com>
2021-12-04 10:49:56 -0300

    primeiro teste swagger - rodando

--- src/main/java/br/com/panacademy/bluebank/config/SwaggerConfig.java
+++
@@ -1,4 +0,0 @@
-package br.com.panacademy.bluebank.config;
-
-public class SwaggerConfig {
-}
--- src/main/java/br/com/panacademy/bluebank/controle/ContaControle.java
+++ src/main/java/br/com/panacademy/bluebank/controle/ContaControle.java
@@ -2,6 +2,8 @@ package br.com.panacademy.bluebank.controle;
 
 import br.com.panacademy.bluebank.dto.ContaDTO;
 import br.com.panacademy.bluebank.servico.ContaServico;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,8 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/contas")
+@Api(value="API REST BlueBank")
+@CrossOrigin(origins = "*")
 public class ContaControle {
 
  private final ContaServico contaServico;
@@ -22,18 +26,21 @@ public class ContaControle {
     }
 
     @GetMapping
+    @ApiOperation("Lista todas as contas")
     public ResponseEntity<List<ContaDTO>> listarTodasAsContas(){
         List<ContaDTO> listaContaDTO = contaServico.listarTodas();
         return ResponseEntity.ok(listaContaDTO);
     }
 
-    @GetMapping(value = "/{id}") // GET /contas/{id}
+    @GetMapping(value = "/{id}")
+    @ApiOperation("Busca uma conta pelo ID")
     public ResponseEntity<ContaDTO> filtrarContaPorId(@PathVariable Long id) {
         ContaDTO conta = contaServico.filtrarContaPorId(id);
         return ResponseEntity.ok(conta);
     }
 
     @DeleteMapping(value = "/{id}")
+    @ApiOperation("Deleta uma conta")
     public ResponseEntity<Void> deletar(@PathVariable Long id) {
         contaServico.deletarConta(id);
         return ResponseEntity.noContent().build();
