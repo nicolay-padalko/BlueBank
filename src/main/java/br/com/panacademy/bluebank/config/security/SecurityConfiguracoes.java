1bcf28f5217693e00c1c4684e63384dc84c7d69d
Chrystian Oliveira <chrystianmoliveira7@gmail.com>
2021-12-10 00:12:07 -0300

    att

--- src/main/java/br/com/panacademy/bluebank/config/security/SecurityConfiguracoes.java
+++ src/main/java/br/com/panacademy/bluebank/config/security/SecurityConfiguracoes.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -38,40 +39,54 @@ public class SecurityConfiguracoes extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers(HttpMethod.POST,"/auth").permitAll()
-                .antMatchers(HttpMethod.GET,"/clientes").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET,"/clientes/*").hasRole("ADMIN")
-                .antMatchers(HttpMethod.POST,"/clientes").permitAll()
-                .antMatchers(HttpMethod.DELETE,"/clientes/*").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PUT,"/clientes").hasRole("CLIENTE")
-                .antMatchers(HttpMethod.PUT,"/clientes/*").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PUT,"/clientes/credenciais/*").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET,"/contas").hasRole("CLIENTE")
-                .antMatchers(HttpMethod.GET,"/contas/*").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET,"/funcionarios").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET,"/funcionarios/*").hasRole("ADMIN")
-                .antMatchers(HttpMethod.POST,"/funcionarios").hasRole("ADMIN")
-                .antMatchers(HttpMethod.DELETE,"/funcionarios/*").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PUT,"/funcionarios/*").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PUT,"/funcionarios/credenciais/*").hasRole("ADMIN")
-                .antMatchers(HttpMethod.POST,"/funcionarios/cadastrarEmail/*").hasRole("ADMIN")
-                .antMatchers(HttpMethod.POST,"/perfis").hasRole("ADMIN")
-                .antMatchers(HttpMethod.DELETE,"/perfis").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET,"/transacoes").hasRole("CLIENTE")
-                .antMatchers(HttpMethod.POST,"/transacoes/depositar").hasRole("CLIENTE")
-                .antMatchers(HttpMethod.POST,"/transacoes/sacar").hasRole("CLIENTE")
-                .antMatchers(HttpMethod.POST,"/transacoes/transferir/*").hasRole("CLIENTE")
-                .antMatchers(HttpMethod.POST,"/transacoes/depositar/*").hasRole("ADMIN")
-                .antMatchers(HttpMethod.POST,"/transacoes/sacar/*").hasRole("ADMIN")
-                .antMatchers(HttpMethod.POST,"/transacoes/transferir/*/*").hasRole("ADMIN")
-                .antMatchers("/**").permitAll()
-                .anyRequest().authenticated()
-                .and().csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().addFilterBefore(new AutenticacaoViaTokenFiltro(tokenServico, usuarioRepositorio), UsernamePasswordAuthenticationFilter.class);
-    }
+    public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            web.ignoring().antMatchers("/v2/api-docs",
+                    "/configuration/ui",
+                    "/swagger-resources/**",
+                    "/configuration/security",
+                    "/swagger-ui.html",
+                    "/webjars/**");
+        }
+
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .antMatchers(HttpMethod.POST, "/auth").permitAll()
+                    .antMatchers(HttpMethod.GET, "/clientes").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.GET, "/clientes/*").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.POST, "/clientes").permitAll()
+                    .antMatchers(HttpMethod.DELETE, "/clientes/*").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.PUT, "/clientes").hasRole("CLIENTE")
+                    .antMatchers(HttpMethod.PUT, "/clientes/*").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.PUT, "/clientes/credenciais/*").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.GET, "/contas").hasRole("CLIENTE")
+                    .antMatchers(HttpMethod.GET, "/contas/*").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.GET, "/funcionarios").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.GET, "/funcionarios/*").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.POST, "/funcionarios").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.DELETE, "/funcionarios/*").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.PUT, "/funcionarios/*").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.PUT, "/funcionarios/credenciais/*").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.POST, "/funcionarios/cadastrarEmail/*").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.POST, "/perfis").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.DELETE, "/perfis").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.GET, "/transacoes").hasRole("CLIENTE")
+                    .antMatchers(HttpMethod.POST, "/transacoes/depositar").hasRole("CLIENTE")
+                    .antMatchers(HttpMethod.POST, "/transacoes/sacar").hasRole("CLIENTE")
+                    .antMatchers(HttpMethod.POST, "/transacoes/transferir/*").hasRole("CLIENTE")
+                    .antMatchers(HttpMethod.POST, "/transacoes/depositar/*").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.POST, "/transacoes/sacar/*").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.POST, "/transacoes/transferir/*/*").hasRole("ADMIN")
+                    .anyRequest().denyAll()
+                    .and().csrf().disable()
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and().addFilterBefore(new AutenticacaoViaTokenFiltro(tokenServico, usuarioRepositorio), UsernamePasswordAuthenticationFilter.class);
+        }
+    }
 }
+
+
